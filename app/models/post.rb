f5e1171c8724d1ad28d8be138464292c4668ab1a
shivani <shivani.yuvasoft290@gmail.com>
2020-08-13 17:58:02 +0530

    add_mongodb_image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Paperclip
   field :title, type: String
   field :body, type: String
   has_many :comments, dependent: :destroy
+  has_mongoid_attached_file  :avatar, :styles => { :large => "380x380!" , :medium => "240x240", :small => "120x120!" }
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
